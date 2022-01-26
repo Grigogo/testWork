4c712e78397ed05372f390fc4d260f44d64fd1d0
Grigogo <grigoriirozhkov@gmail.com>
2022-01-26 19:43:03 +0300

    first commit3

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,16 +4,20 @@ const price = document.querySelectorAll('.cards__item-price')
 
 const text = document.querySelectorAll('.cards__item-price span')
 
-let summa = document.querySelector('footer__sum span')
+let summa = document.querySelector('.footer__sum span')
 
-console.log(text[0].textContent)
+const textAdd = document.querySelectorAll('.cards__item-button')
+
+
 const changeClass = (indexClickedTabs) => {
     buttons.forEach((tab, index) => {
         if (index === indexClickedTabs) {
-            tab.classList.add('clicked')
+            tab.classList.add('clicked') 
+            textAdd[index].textContent = `Добавлено`
         }
     })
 }
+
 
 const sumPrice = (index) => {
     let priceText = Number(text[index].textContent)
@@ -21,9 +25,10 @@ const sumPrice = (index) => {
 }
 
 const showSum = () => {
-    summa.textContent = String(sum)
-    console.log(summa.textContent)
+    summa.textContent = `ОБЩАЯ СУММА: ${sum} руб`
 }
+
+
 
 buttons.forEach((tap, index) => {
     tap.addEventListener('click', () => {
